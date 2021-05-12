65931f81d88edb4d9f2b54ef60a892dc62c6afb0
Your Name <you@example.com>
2021-05-12 21:15:41 +0900

    画面遷移

--- spring_first1/src/main/java/com/example/demo/controller/ConfirmController.java
+++ spring_first1/src/main/java/com/example/demo/controller/ConfirmController.java
@@ -1,5 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+@RequestMapping("/confirm")
 public class ConfirmController {
+	@GetMapping
+	String geteConfirm(@RequestParam(name = "question") String q) {
+
+		return "edit";
+	}
 
 }
--- spring_first1/src/main/java/com/example/demo/controller/EditController.java
+++ spring_first1/src/main/java/com/example/demo/controller/EditController.java
@@ -1,5 +1,39 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.Answer;
+import com.example.demo.entity.Question;
+import com.example.demo.service.AnswerService;
+import com.example.demo.service.QuestionService;
+
+@Controller
+
+@RequestMapping("/edit")
 public class EditController {
+	@Autowired
+	private QuestionService questionService;
+	@Autowired
+	private AnswerService answerService;
+
+	@GetMapping
+	String geteEit(@RequestParam(name = "question") String q, Model model) {
+
+		int question_id = Integer.parseInt(q);
+		//クエスチョンの取得
+		Question question = questionService.findId(question_id);
+		List<Answer> listA = answerService.findAnswer(question_id);
+		model.addAttribute("question", question);
+		model.addAttribute("answerList", listA);
+
+		return "edit";
+	}
 
 }
